ae34daa8403a5cb7fdebc347486dfd0812a4e92a
John8Rider <connerrobert927@gmail.com>
2020-11-26 22:36:05 +0530

    Added More Game Types in the API call

--- src/api.js
+++ src/api.js
@@ -32,5 +32,9 @@ const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 //Popular Games
 
 const popular_games = `games?dates=${lastYear},${currnetDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?dates=${currnetDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?dates=${lastYear},${currnetDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => ` ${base_url}${newGames}`;
