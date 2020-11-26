3320aab6873466a3cfb83aea4838843a502878ec
John8Rider <connerrobert927@gmail.com>
2020-11-26 22:03:00 +0530

    Setup Redux Store

---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,16 @@
+const initState = {
+  popular: [],
+  newGames: [],
+  upcoming: [],
+  searched: [],
+};
+const gamesReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "FETCH_GAMES":
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
+
+export default gamesReducer;
