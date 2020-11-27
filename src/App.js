b9265383270a85a39b4b96e17ad02c0353132528
John8Rider <connerrobert927@gmail.com>
2020-11-27 05:33:19 +0530

    Added More Game Sections and Global Styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 // Componets and Pages
 import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,13 +16,37 @@ const Home = () => {
     dispatch(loadGames());
   }, [dispatch]);
   //Get that data back
-  const { popular, newGmaes, upcoming } = useSelector((state) => state.games);
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
 
   return (
     <GameList>
       <h2>Upcoming Games</h2>
       <Games>
         {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+      <h2>Popular Games</h2>
+      <Games>
+        {popular.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+      <h2>New Games</h2>
+      <Games>
+        {newGames.map((game) => (
           <Game
             name={game.name}
             released={game.released}
